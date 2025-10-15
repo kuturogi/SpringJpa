153c82ad12714ae55cbdf5b2fabd04335d4dd494
Bertuğ Kuturoğlu <bkuturoglu@icloud.com>
2025-10-15 21:23:16 +0300

    Database Connection and JPA Project Initialization

--- spring-jpa/src/main/java/com/bertugkuturoglu/spring_jpa/SpringJpaApplication.java
+++ spring-jpa/src/main/java/com/bertugkuturoglu/spring_jpa/SpringJpaApplication.java
@@ -2,7 +2,9 @@ package com.bertugkuturoglu.spring_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.bertugkuturoglu"})
 @SpringBootApplication
 public class SpringJpaApplication {
 
