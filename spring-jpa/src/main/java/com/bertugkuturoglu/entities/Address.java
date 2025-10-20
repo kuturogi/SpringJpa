0c8793edd26e3e1e1bab89b77b4df90af5d7de1f
Bertuğ Kuturoğlu <bkuturoglu@icloud.com>
2025-10-20 23:31:13 +0300

    Created Address And Customer Entitites and connected OnetoOne Relations with each other

---
+++ spring-jpa/src/main/java/com/bertugkuturoglu/entities/Address.java
@@ -0,0 +1,28 @@
+package com.bertugkuturoglu.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "adress")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(mappedBy = "address")
+    private Customer customer;
+
+
+}
---
+++ spring-jpa/src/main/java/com/bertugkuturoglu/entities/Customer.java
@@ -0,0 +1,28 @@
+package com.bertugkuturoglu.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne
+    private Address address;
+
+
+}
