a3429b534d9fb9d53aed9dfe85d56ae74eeabb9c
Bertuğ Kuturoğlu <bkuturoglu@icloud.com>
2025-10-20 23:55:25 +0300

    Completed Controller Layer For FindByID

--- spring-jpa/src/main/java/com/bertugkuturoglu/controller/ICustomerController.java
+++ spring-jpa/src/main/java/com/bertugkuturoglu/controller/ICustomerController.java
@@ -1,6 +1,8 @@
 package com.bertugkuturoglu.controller;
 
+import com.bertugkuturoglu.dto.DtoCustomer;
+
 public interface ICustomerController {
 
-
+    public DtoCustomer findCustomerById(Long id);
 }
--- spring-jpa/src/main/java/com/bertugkuturoglu/controller/impl/CustomerControllerImpl.java
+++ spring-jpa/src/main/java/com/bertugkuturoglu/controller/impl/CustomerControllerImpl.java
@@ -1,6 +1,11 @@
 package com.bertugkuturoglu.controller.impl;
 
 import com.bertugkuturoglu.controller.ICustomerController;
+import com.bertugkuturoglu.dto.DtoCustomer;
+import com.bertugkuturoglu.services.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/api/customer")
 public class CustomerControllerImpl implements ICustomerController {
 
+        @Autowired
+        private ICustomerService customerService;
 
+    @GetMapping(path = "/list/{id}")
+    @Override
+    public DtoCustomer findCustomerById(@PathVariable(name = "id") Long id) {
+        return customerService.findCustomerById(id);
+    }
 }
